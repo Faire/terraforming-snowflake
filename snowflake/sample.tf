168169056c68141f832c659874f69ca5923d4d58
pete-repete <peter.vandermeer@faire.com>
2022-07-11 17:44:19 -0400

    add sample terraform file

---
+++ snowflake/sample.tf
@@ -0,0 +1,6 @@
+resource snowflake_database "this_is_a_test" {
+  name = "this_is_a_test"
+  description = "you can delete this, it's just an example"
+  // if no provider is specified, default is used. See main.tf for default
+  provider = snowflake.security_admin
+}
